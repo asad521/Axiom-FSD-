4222cf82bffc598833b680d9aded29136782154d
asad521 <63185364+asad521@users.noreply.github.com>
2021-04-15 02:16:49 +0500

    Icon Additions and Conditions
    Following functions  are added:
    checkRequired()
    checkLength()
    checkEmail()
    checkPasswordsMatch

--- Project 1/testscript.js
+++ Project 1/testscript.js
@@ -3,7 +3,7 @@ const form =document.getElementById("form");
 const username =document.getElementById("username");
 const email =document.getElementById('email');
 const password=document.getElementById("password");
-const password2=document.getElementById("password2");
+const password2=document.getElementById("password confirmation");
 
 
 
@@ -32,48 +32,87 @@ function showSuccess(input){
 
 
 
+form.addEventListener('submit',function(e){
 
-/////////////////Evenet Listener  & Condition on Listener//////////////////////////
-// Create Event listener for submist button
-// form.addEventListener('event_name', action-function)
-form.addEventListener('submit',function (e)
-{
-    e.preventDefault();     //Stop page from reloading
-    // console.log(username);
-    //Condition//
-    // condition for  username
-    if (username.value==='')
-    {
-        showError(username,'Username is required');
-    }
-    else {
-        showSuccess(username);
-    }  
+    e.preventDefault();
+    
+    const elements=document.getElementsByTagName("input");
+    // console.log(elements);
+    checkRequired(elements);
+    // console.log(output);
+    checkLength(elements);
+    checkEmail(elements);
+    checkConfirmation(elements  );
 
-    // condition for  email
-    if (email.value==='')
-    {
-        showError(email,'Email is required');
-    }
-    else {
-        showSuccess(email);
-    }  
-        // condition for  password
-    if (password.value==='')
-    {
-        showError(password,'Password is required');
-    }
-    else {
-        showSuccess(password);
-    }  
-    // condition for  password2
-    if (password2.value==='')
-    {
-        showError(password2,'Confrimation is required');
-    }
-    else {
-        showSuccess(password2);
-    }  
-});
+} )
 
- 
+    // Check If any Fields are Empty 
+    function checkRequired(elements){
+        var output={}
+        for (i=0; i<elements.length; i++){
+            if (elements[i].value === '') {
+                output[elements[i].id]=false;
+                showError(elements[i],elements[i].id +' must be fiilled.') 
+            }
+        }
+        return output;
+
+    }
+    //Check Length of Password and User Name
+    function checkLength(elements){
+
+        for (i=0; i<elements.length; i++) {
+            if (elements[i].id ==='username' || elements[i].id === 'password'){
+                if (elements[i].value !== '')   {
+                    if (elements[i].value.length < 5) {
+                        showError(elements[i],elements[i].id + "is less than 5")
+                    } else if (elements[i].value.length > 15) {
+                        showError(elements[i],elements[i].id + "is greater than 15")
+                    } else {
+                        showSuccess(elements[i])
+                    }
+                    
+                }
+            }
+        }
+    }
+    // Check If email is valid  valid--> a@yahoo.com
+    function checkEmail(elements) {
+        console.log("email condition");
+        for (i=0; i<elements.length; i++) {
+        console.log(elements[i]);
+            if (elements[i].id ==='email') {
+                if (elements[i].value !=''){
+                    var regex = /^[^@]+@(yahoo|hotmail|outlook|gmail)\.(com|in|co\.uk)$/i;
+                    var str = elements[i].value;
+                    var result = regex.test(str);
+                    if (result!=true)
+                    {
+                    showError(elements[i],elements[i].id+" is not valid.") }
+                                    
+                    else {
+                        showSuccess(elements[i])
+                                        }
+                }
+        }        
+    }
+
+   }
+   //Check If password confirmation match.
+   function checkConfirmation(elements) {
+
+       for(i=0; i<elements.length; i++){
+           if (elements[i].id=== 'password confirmation') {
+              if (elements[i-1].value ===''){
+                  showError(elements[i]," Please Set the Password First")
+              }     else if (elements[i].value !== elements[i-1].value) {
+                    showError(elements[i],"Password did not match.")
+                    } else {
+                        showSuccess(elements[i]);
+                        showSuccess(elements[i-1]);
+                    }
+            }
+        }
+    }
+   
+
